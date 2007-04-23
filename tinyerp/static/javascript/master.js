2ad532aa5ca54bfaebbc37d6c6c4bcd87e3461b8
Amit Mendapara <ame@tinyerp.com>
2007-04-23 05:12:50 +0000

    * Cleanup master.js

--- tinyerp/static/javascript/master.js
+++ tinyerp/static/javascript/master.js
@@ -28,63 +28,10 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
- * Configuration settings for TinyAjax
- *
- */
-var TinyAjaxConfig = {
-	status: "Loading...",			// The message to display
-	statusElem: "load_status",		// The element id in which message should be display
-	target: null					// Target element id where result should go
-}
-
-/**
- * A wrapper arround MochiKit's doSimpleXMLHttpRequest method.
- * Provides a way to display Google style fancy "Loading..." message.
- *
- */
-var TinyAjax = {
-
-	/**
-	 * A wrapper arround doSimpleXMLHttpRequest
-	 *
-	 * @param url the url
-	 * @param params request paramas as map
-	 * @param update the element id where result should go
-	 * @param message the loading message
-	 * @return differed object with wait(sec) function
-	 *
-	 */
-	request: function(url, params, update, message) {
-
-		$(TinyAjaxConfig.statusElem).innerHTML = message ? message : TinyAjaxConfig.status;
-		$(TinyAjaxConfig.statusElem).style.display = "block";
-
-		res = doSimpleXMLHttpRequest(url, params);
-
-		res.addCallback(function(xmlHttp){
-			target = update ? update : TinyAjaxConfig.target;
-			if (target) {
-				$(target).innerHTML = xmlHttp.responseText;
-			}
-		});
-
-		res.addBoth(function(xmlHttp){
-			$(TinyAjaxConfig.statusElem).style.display = "none";
-		});
-
-		res.wait = function(sec){
-			return wait(sec, res);
-		}
-
-		return res;
-	}
-}
-
-/**
  * A function to open centered popup window
  *
  */
-function wopen(url, name, w, h) {
+var wopen = function(url, name, w, h) {
     var width = w ? w : 400;
     var height = h ? h : 300;
     var left = parseInt((screen.availWidth/2) - (width/2));
@@ -95,6 +42,6 @@ function wopen(url, name, w, h) {
     return window.open(url, name ? name : "popup", windowFeatures);
 }
 
-function getURL(path, args) {
+var getURL = function(path, args) {
 	return args ? path + "?" + queryString(args) : path;
 }
