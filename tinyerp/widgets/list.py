32d0feef38af3eb60f96b041230386880b01722a
Amit Mendapara <ame@tinyerp.com>
2007-06-03 23:49:59 +0000

    * Fixed list widget (Integer formating problem in links).

--- tinyerp/widgets/list.py
+++ tinyerp/widgets/list.py
@@ -149,7 +149,7 @@ class List(TinyCompoundWidget):
             for row in data:
                 cell = row[name]
                 cell.link = "javascript: void(0)"
-                cell.onclick = "do_select(%d)"%(row['id'])
+                cell.onclick = "do_select(%s); return false;"%(row['id'].text)
                 
         return headers, data    
 
