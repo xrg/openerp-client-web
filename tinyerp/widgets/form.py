b6a65414dd760888ffea292f51798fdbda5fda7c
Amit Mendapara <ame@tinyerp.com>
2007-07-06 01:23:16 +0000

    * Improved Integer, Float widgets.

--- tinyerp/widgets/form.py
+++ tinyerp/widgets/form.py
@@ -261,6 +261,9 @@ class Integer(TinyField):
     def __init__(self, attrs={}):
         super(Integer, self).__init__(attrs)
         self.validator = tiny_validators.Int()
+        
+        if not self.default:
+            self.default = 0
 
     def set_value(self, value):
         if value:
@@ -282,6 +285,9 @@ class Float(TinyField):
     def __init__(self, attrs={}):
         super(Float, self).__init__(attrs)
         self.validator = tiny_validators.Float()
+        
+        if not self.default:
+            self.default = 0.0
 
     def set_value(self, value):
         self.default = value
