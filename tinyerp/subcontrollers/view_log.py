27441a07e1b71629be100857ecb25146ce7850df
Navrang Oza <noz@tinyerp.com>
2007-08-22 04:22:50 +0000

    * Set Property.

--- tinyerp/subcontrollers/view_log.py
+++ tinyerp/subcontrollers/view_log.py
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2004-2006 TINY SPRL. (http://tiny.be) All Rights Reserved.
 #
-# $Id $
+# $Id$
 #
 # WARNING: This program as such is intended to be used by professional
 # programmers who take the whole responsability of assessing all potential
