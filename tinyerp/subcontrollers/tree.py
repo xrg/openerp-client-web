a8591eb3df4a84d633cca190b7c0402ac78bdecb
Amit Mendapara <ame@tinyerp.com>
2007-11-06 05:00:15 +0000

    * Fixed `TreeView` (missing action attr)

--- tinyerp/subcontrollers/tree.py
+++ tinyerp/subcontrollers/tree.py
@@ -72,7 +72,7 @@ class Tree(controllers.Controller, TinyResource):
         else:
             view = cache.fields_view_get(model, False, 'tree', context)
 
-        tree = tree_view.ViewTree(view, model, res_id, domain=domain, context=context)
+        tree = tree_view.ViewTree(view, model, res_id, domain=domain, context=context, action="/tree/action")
         if tree.toolbar:
             for tool in tree.toolbar:
                 tool['icon'] = icons.get_icon(tool['icon'])
