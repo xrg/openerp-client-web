0368ca53b2661250b306212b936b181cbfb371e0
Navrang Oza <noz@tinyerp.com>
2007-11-23 05:40:11 +0000

    * Removed extra import statements.

--- tinyerp/subcontrollers/attachment.py
+++ tinyerp/subcontrollers/attachment.py
@@ -28,25 +28,19 @@
 ###############################################################################
 
 import os
-import time
 import base64
 
 from turbogears import expose
 from turbogears import controllers
 
-import cherrypy
-
 from tinyerp import rpc
-from tinyerp import tools
-from tinyerp import common
-
 from tinyerp.tinyres import TinyResource
 from tinyerp.utils import TinyDict
 
 import tinyerp.widgets as tw
 
 class Attachment(controllers.Controller, TinyResource):
-    
+
     @expose(template="tinyerp.subcontrollers.templates.attachment")
     def index(self, model, id):
 
@@ -55,20 +49,20 @@ class Attachment(controllers.Controller, TinyResource):
         params = TinyDict()
         params.model = 'ir.attachment'
         params.view_mode = ['tree', 'form']
-        
+
         params.domain = [('res_model', '=', model), ('res_id', '=', id)]
 
         screen = tw.screen.Screen(params, selectable=1)
         screen.widget.pageable = False
 
         return dict(screen=screen, model=model, id=id, show_header_footer=False)
-    
+
     @expose()
     def add(self, model, id, uploadfile):
-                
-        data = uploadfile.file.read()                
+
+        data = uploadfile.file.read()
         fname = os.path.basename(uploadfile.filename)
-                
+
         # XXX: we can't reconise basename of window path on Linux
         if '\\' in fname: # though `\` is valid in Unix path
             fname = fname.split('\\')[-1]
@@ -76,16 +70,16 @@ class Attachment(controllers.Controller, TinyResource):
         if data:
             proxy = rpc.RPCProxy('ir.attachment')
             proxy.create({'name': fname, 'datas': base64.encodestring(data), 'datas_fname': fname, 'res_model': model, 'res_id': id})
-        
+
         return self.index(model, id)
-    
+
     @expose()
     def delete(self, model, id, record, **kw):
         record = int(record)
-        
+
         proxy = rpc.RPCProxy('ir.attachment')
         proxy.unlink([record])
-        
+
         return self.index(model, id)
 
     @expose(content_type="application/octat-stream")
@@ -96,6 +90,6 @@ class Attachment(controllers.Controller, TinyResource):
         data = proxy.read([record])
 
         if len(data) and not data[0]['link'] and data[0]['datas']:
-            return base64.decodestring(data[0]['datas'])        
+            return base64.decodestring(data[0]['datas'])
         else:
             return ''
