c69b5febafcc430e5d02d7e3579e406c3eaeb246
Amit Mendapara <ame@tinyerp.com>
2008-05-08 06:12:59 +0000

    * Fixed M2M (view processing error).

--- tinyerp/widgets/many2many.py
+++ tinyerp/widgets/many2many.py
@@ -65,16 +65,16 @@ class M2M(TinyField, tg.widgets.CompoundWidget):
         self.domain = attrs.get('domain', [])
         self.context = attrs.get('context', {}) or {}
 
-        self.view = attrs.get('views',{})
+        self.views = attrs.get('views', {})
         self.domain  = attrs.get('domain',{})
         self.ids = attrs.get('value') or []
 
-        if not self.view:
+        if not self.views:
             ctx = rpc.session.context.copy()
             ctx.update(self.context)
-            self.view = cache.fields_view_get(self.relation, {}, 'tree', ctx, False)
+            self.views = cache.fields_view_get(self.relation, {}, 'tree', ctx, False)
 
-        self.list_view = List(self.name, self.relation, self.view, ids=self.ids, domain=self.domain, context=self.context, selectable=(self.editable or 0) and 2, pageable=False)
+        self.list_view = List(self.name, self.relation, self.views.get('tree', {}), ids=self.ids, domain=self.domain, context=self.context, selectable=(self.editable or 0) and 2, pageable=False)
         self.list_view.show_links = -1
 
         self.validator = tiny_validators.many2many()
