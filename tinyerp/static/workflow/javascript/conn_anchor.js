fb26e60bf5b6cf09a634245f2c0bb7f098500a35
Shanta Kabra <ksh@tinyerp.com>
2008-06-26 05:52:25 +0000

    * implemented connection anchor for vertical parallel connectors case

--- tinyerp/static/workflow/javascript/conn_anchor.js
+++ tinyerp/static/workflow/javascript/conn_anchor.js
@@ -39,67 +39,71 @@ getLocation : function(/*:draw2d.Point*/ reference)
 			if(connectors.get(i).tr_id==this.conn_id)
 				var conn = connectors.get(i);
 				
-		if(conn.isOverlaping) {		
+		//multiple connectors		
+		if(conn.isOverlaping) {	
 			
-			
-			//horizontal parallel lines
-			if(ry==0) {				
-				if(conn.OverlapingSeq%2==0)
-					return new draw2d.Point(Math.round(center.x), Math.round(center.y + (10*conn.OverlapingSeq)));
-				else
-					return new draw2d.Point(Math.round(center.x), Math.round(center.y - (10*conn.OverlapingSeq)));
-			}//vertical parallel lines			
-			else if(rx==0) {
-				if(conn.OverlapingSeq%2==0)
-					return new draw2d.Point(Math.round(center.x + (15*conn.OverlapingSeq)), Math.round(center.y));
-				else
-					return new draw2d.Point(Math.round(center.x - (15*conn.OverlapingSeq)), Math.round(center.y));
+			//vertical parallel lines
+			if(rx==0) {
+				var x = bounds.w / 2;
+				var y = 0;		
+			}//horizontal parallel lines			
+			else if(ry==0) {
+				var x = 0;
+				var y = bounds.h / 2;
 			} 
 			else {
 				
 				var m = -1/slope; //slope of perpendicular line  is negative of resiprocal of slope				
 				
 				//solving equation of ellipse and line which is prependicular to the line passing through two center 
-				var x = Math.sqrt((a2*b2)/(b2+(m*m*a2)));
-				var y = m*x;	
-				var k = 1/(conn.totalOverlaped+1);
-				
-				var xd = -2*x;
-				var yd = -2*y;					
-				
-				var xnew = x + (k*xd*conn.OverlapingSeq);
-				var ynew = y + (k*yd*conn.OverlapingSeq);
-				
+				var x = Math.sqrt((a2 * b2) / (b2 + (m * m * a2)));
+				var y = m * x;		
 			}
-		}else {
 			
+			var xd = -2 * x;
+			var yd = -2 * y;	
+			var k = 1/(conn.totalOverlaped + 1);				
+			
+			var xnew = x + (k * xd * conn.OverlapingSeq); 
+			var ynew = y + (k * yd * conn.OverlapingSeq);
+		}
+		else {//single connector
 			var xnew = 0;
 			var ynew = 0;			
 		}
 		
 		//find point on ellipse
-		var c = ynew - (slope * xnew);
-				
-		var A = (b2) + ((a2) * (slope*slope));
-		var B = 2 * c * slope * (a2);
-		var C = (a2) * ((c*c) - (b2));
+		if(rx!=0) {
+			
+			var c = ynew - (slope * xnew);
+					
+			var A = (b2) + ((a2) * (slope*slope));
+			var B = 2 * c * slope * (a2);
+			var C = (a2) * ((c*c) - (b2));
+			
+			var discriminator = Math.sqrt((B*B) - (4 * A *C));
+			
+			var root1x = (-B + discriminator)/(2 * A);
+			var root2x = (-B - discriminator)/(2 * A);
+			
+			//substituting x in y=mx+c
+			var root1y = slope*root1x + c;
+			var root2y = slope*root2x + c;
+		}
+		else {			
+			var root1x = xnew;
+			var root2x = xnew;
+			
+			var root1y = Math.sqrt((b2 * (a2 - (root1x*root1x)))/(a2))
+			var root2y = -root1y;
+		}
 		
-		var discriminator = Math.sqrt((B*B) - (4 * A *C));
-		
-		var root1x = (-B + discriminator)/(2*A);
-		var root2x = (-B - discriminator)/(2*A);
-		
-		var root1y = slope*root1x + c;
-		var root2y = slope*root2x + c;
-						
-		
-		var dist1 = Math.sqrt(((reference.x-(center.x+root1x))*(reference.x-(center.x+root1x))) + ((reference.y-(center.y+root1y))*(reference.y-(center.y+root1y))));
-		var dist2 = Math.sqrt(((reference.x-(center.x+root2x))*(reference.x-(center.x+root2x))) + ((reference.y-(center.y+root2y))*(reference.y-(center.y+root2y))));
+		var dist1 = Math.sqrt(((rx-root1x)*(rx-root1x)) + ((ry-root1y)*(ry-root1y)));
+		var dist2 = Math.sqrt(((rx-root2x)*(rx-root2x)) + ((ry-root2y)*(ry-root2y)));
 
 		if(dist2>dist1)
 			return new draw2d.Point(Math.round(center.x + root1x), Math.round(center.y + root1y));
 		else
 			return new draw2d.Point(Math.round(center.x + root2x), Math.round(center.y + root2y));
-
 	},
 });
