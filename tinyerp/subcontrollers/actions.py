11b8499b8deb0f3a12f25d77297c0ba07850ce2b
Christophe Simonis <christophe@tinyerp.com>
2008-09-09 15:22:49 +0200

    ir.actions.act_window get a binary field

--- tinyerp/subcontrollers/actions.py
+++ tinyerp/subcontrollers/actions.py
@@ -322,7 +322,11 @@ def execute_by_id(act_id, type=None, **data):
 
     if type==None:
         type = get_action_type(act_id)
-
+	
+	ctx = rpc.session.context.copy()
+    if type == 'ir.actions.act_window':
+		# the field 'views' is transfered as a binary field
+        ctx['get_binary_size'] = False
     res = rpc.session.execute('object', 'execute', type, 'read', [act_id], False, rpc.session.context)[0]
     return execute(res, **data)
 
