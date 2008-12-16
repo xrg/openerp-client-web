ddd819a637f419ba54f0f44396a85015ec2d8188
Stephane Wirtel <stephane@tinyerp.com>
2008-12-16 19:29:50 +0100

    [IMP] Use optparse for the command line

--- openerp/commands.py
+++ openerp/commands.py
@@ -4,13 +4,14 @@
 import os
 import sys
 
+from os.path import exists
+
 # ubuntu 8.04 has obsoleted `pyxml` package and installs here.
 # the path needs to be updated before any `import xml`
 _oldxml = '/usr/lib/python%s/site-packages/oldxml' % sys.version[:3]
-if os.path.exists(_oldxml):
+if exists(_oldxml):
     sys.path.append(_oldxml)
 
-from os.path import dirname, exists, join
 
 import pkg_resources
 pkg_resources.require("TurboGears")
@@ -23,26 +24,10 @@ cherrypy.lowercase_api = True
 class ConfigurationError(Exception):
     pass
 
-def start():
+def start(configfile=None):
     """Start the CherryPy application server."""
 
-    setupdir = dirname(dirname(__file__))
-    curdir = os.getcwd()
-
-    # First look on the command line for a desired config file,
-    # if it's not on the command line, then look for 'setup.py'
-    # in the current directory. If there, load configuration
-    # from a file called 'dev.cfg'. If it's not there, the project 
-    # is probably installed and we'll look first for a file called
-    # 'prod.cfg' in the current directory and then for a default
-    # config file called 'default.cfg' packaged in the egg.
-    if len(sys.argv) > 1:
-        configfile = sys.argv[1]
-    elif exists(join(setupdir, "setup.py")):
-        configfile = join(setupdir, "dev.cfg")
-    elif exists(join(curdir, "prod.cfg")):
-        configfile = join(curdir, "prod.cfg")
-    else:
+    if configfile is None:
         try:
             configfile = pkg_resources.resource_filename(
               pkg_resources.Requirement.parse("openerp-web"), 
@@ -50,11 +35,10 @@ def start():
         except pkg_resources.DistributionNotFound:
             raise ConfigurationError(_("Could not find default configuration."))
 
-    if not os.path.exists(configfile):
+    if not exists(configfile):
         raise ConfigurationError(_("Could not find configuration file: %s") % configfile)
 
-    turbogears.update_config(configfile=configfile,
-        modulename="openerp.config")
+    turbogears.update_config(configfile=configfile, modulename="openerp.config")
     
     # save the name of the configfile (TODO: config editor)
     import openerp
--- start-openerp-web.py
+++ start-openerp-web.py
@@ -7,12 +7,38 @@ directory. When the project is installed, easy_install will create a
 proper start script.
 """
 
+import os
 import sys
+import optparse
+from os.path import join, dirname, exists
 from openerp.commands import start, ConfigurationError
+from openerp.release import version
+
+def get_config_file():
+    configfile = None
+    setupdir = dirname(dirname(__file__))
+    curdir = os.getcwd()
+    if exists(join(setupdir, "setup.py")):
+        configfile = join(setupdir, "dev.cfg")
+    elif exists(join(curdir, "prod.cfg")):
+        configfile = join(curdir, "prod.cfg")
+    return configfile
 
 if __name__ == "__main__":
     try:
-        start()
+        # First look on the command line for a desired config file,
+        # if it's not on the command line, then look for 'setup.py'
+        # in the current directory. If there, load configuration
+        # from a file called 'dev.cfg'. If it's not there, the project 
+        # is probably installed and we'll look first for a file called
+        # 'prod.cfg' in the current directory and then for a default
+        # config file called 'default.cfg' packaged in the egg.
+        parser = optparse.OptionParser(version=version)
+        parser.add_option("-c", "--config", dest="config", help="specify alternate config file",
+                          default=get_config_file())
+        (opt, args) = parser.parse_args()
+
+        start(opt.config)
     except ConfigurationError, exc:
         sys.stderr.write(str(exc)+"\n")
         sys.exit(1)
