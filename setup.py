56fcc1ed20a49106e4b8270a8fd71bea82e34ad1
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-01-12 19:18:09 +0530

    * Improved setup.py script (dependency check).

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
+import os, sys
+
+# ubuntu 8.04 has obsoleted `pyxml` package and installs here.
+# the path needs to be updated before any `import xml`
+_oldxml = '/usr/lib/python%s/site-packages/oldxml' % sys.version[:3]
+if os.path.exists(_oldxml):
+    sys.path.append(_oldxml)
+    
 from setuptools import setup, find_packages
 from turbogears.finddata import find_package_data
 
-import os
 execfile(os.path.join("openerp", "release.py"))
 
 packages = find_packages()
@@ -15,6 +22,13 @@ data_files = []
 for name in ('config', 'scripts', 'doc'):
     data = find_package_data(where=name, package=name, exclude=('*~',), only_in_packages=False)
     data_files += [(name, [name + '/' + f for f in data[name]])]
+
+# Check for PyXML as it's not compatible with pkg_resources on some systems
+try:
+    exec("import xml.xpath")
+except ImportError:
+    print 'Error: python-xml >= 0.8.4 (PyXML, XML Tools for python) is required.'
+    sys.exit(1)
 
 setup(
     name = "openerp-web",
@@ -30,6 +44,7 @@ setup(
 
     install_requires=[
         "TurboGears >= 1.0.7, < 1.1",
+        "pyparsing >= 1.5.0"
     ],
 
     zip_safe = False,
