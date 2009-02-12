83d006a2df0f2fcb6ed85d29bff03b29373aedca
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-02-12 17:22:39 +0530

    [FIX] easy_install problem.

--- setup.py
+++ setup.py
@@ -5,14 +5,104 @@ import os, sys
 _oldxml = '/usr/lib/python%s/site-packages/oldxml' % sys.version[:3]
 if os.path.exists(_oldxml):
     sys.path.append(_oldxml)
-    
+
+# Check for PyXML as it's not compatible with pkg_resources on some systems
+try:
+    exec("import xml.xpath")
+except ImportError:
+    print 'Error: python-xml >= 0.8.4 (PyXML, XML Tools for python) is required.'
+    sys.exit(1)
+
 from setuptools import setup, find_packages
-from turbogears.finddata import find_package_data
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
 
 execfile(os.path.join("openerp", "release.py"))
 
 if 'bdist_rpm' not in sys.argv:
     version = version + '-' + release
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ('*.py', '*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
+                                './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(
+    where='.', package='',
+    exclude=standard_exclude,
+    exclude_directories=standard_exclude_directories,
+    only_in_packages=True,
+    show_ignored=False):
+    """
+    Return a dictionary suitable for use in ``package_data``
+    in a distutils ``setup.py`` file.
+
+    The dictionary looks like::
+
+        {'package': [files]}
+
+    Where ``files`` is a list of all the files in that package that
+    don't match anything in ``exclude``.
+
+    If ``only_in_packages`` is true, then top-level directories that
+    are not packages won't be included (but directories under packages
+    will).
+
+    Directories matching any pattern in ``exclude_directories`` will
+    be ignored; by default directories with leading ``.``, ``CVS``,
+    and ``_darcs`` will be ignored.
+
+    If ``show_ignored`` is true, then all the files that aren't
+    included in package data are shown on stderr (for debugging
+    purposes).
+
+    Note patterns use wildcards, or can be exact paths (including
+    leading ``./``), and all searching is case-insensitive.
+    """
+
+    out = {}
+    stack = [(convert_path(where), '', package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                    stack.append((fn, '', new_package, False))
+                else:
+                    stack.append((fn, prefix + name + '/', package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
 
 packages = find_packages()
 package_data = find_package_data(where='openerp', package='openerp')
@@ -25,13 +115,6 @@ data_files = []
 for name in ('config', 'scripts', 'doc'):
     data = find_package_data(where=name, package=name, exclude=('*~',), only_in_packages=False)
     data_files += [(name, [name + '/' + f for f in data[name]])]
-
-# Check for PyXML as it's not compatible with pkg_resources on some systems
-try:
-    exec("import xml.xpath")
-except ImportError:
-    print 'Error: python-xml >= 0.8.4 (PyXML, XML Tools for python) is required.'
-    sys.exit(1)
 
 setup(
     name = "openerp-web",
