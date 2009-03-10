fd771e9b77180d6653f9ee31e1b855a3293d4d69
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-03-10 16:27:59 +0530

    [IMP] support for dblist.hide

--- openerp/tinyres.py
+++ openerp/tinyres.py
@@ -127,7 +127,11 @@ def secured(fn):
                     base = re.split('\.|:|/', host)[0]                
                     base = base + '_'                
                     dblist = [d for d in dblist if d.startswith(base)]
-                    
+
+                if config.get('dblist.hide', path='openerp-web'):
+                    db = (dblist or False) and dblist[0]
+                    dblist = None
+
                 return _login(cherrypy.request.path, message=message, dblist=dblist, db=db, 
                         user=user, action=action, origArgs=get_orig_args(kw))
 
