0f000b5acd25a0e33c4a4a204f31b37d88e3aa71
noz <noz@tinyerp.com>
2009-04-08 15:45:27 +0530

    * Fixed m2o problem (If only one record in list).

--- openerp/static/javascript/m2o.js
+++ openerp/static/javascript/m2o.js
@@ -240,13 +240,13 @@ ManyToOne.prototype.get_matched = function(){
     var do_get_matched = function(relation, text, domain, context){
         
         text = m2o.field.value ? '' : text;
-
+        
         var req2 = Ajax.JSON.post('/search/get_matched', {model: relation, text: text, 
                                                          _terp_domain: domain, 
                                                          _terp_context: context});
         
         req2.addCallback(function(obj){
-            if (obj.values.length == 1) {
+            if (text && obj.values.length == 1) {
                 val = obj.values[0];
                 m2o.field.value = val[0];
                 m2o.text.value = val[1];
