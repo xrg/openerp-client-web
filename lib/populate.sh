c7aede1e4032302923b536ab8ddedbcb8cb0d149
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-04-20 16:48:17 +0530

    * supports local lib dir, where one can install dependencies.

---
+++ lib/populate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+test -f 'populate.sh' || exit
+
+PYTHONPATH=. easy_install -a -Z -d . "CherryPy>=3.1.2"
+PYTHONPATH=. easy_install -a -Z -d . "Babel>=0.9.4"
+PYTHONPATH=. easy_install -a -Z -d . "Mako>=0.2.4"
+PYTHONPATH=. easy_install -a -Z -d . "formencode>=1.2.2"
+
+for egg in *.egg
+do
+    if test -d $egg 
+    then
+        rm -rf $egg/EGG-INFO
+        mv $egg/* .
+        rm -rf $egg
+    fi
+done
+
+rm -f site.*
+rm -f easy-install.pth
