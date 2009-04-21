f6da40347dff8257594cde5fe8a47e9973eb3f88
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-04-21 17:36:11 +0530

    * Updated lib/populate.sh

--- lib/populate.sh
+++ lib/populate.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 test -f 'populate.sh' || exit
+test ! -d 'cherrypy' || exit
 
 PYTHONPATH=. easy_install -a -Z -d . "CherryPy>=3.1.2"
 PYTHONPATH=. easy_install -a -Z -d . "Babel>=0.9.4"
 PYTHONPATH=. easy_install -a -Z -d . "Mako>=0.2.4"
+PYTHONPATH=. easy_install -a -Z -d . "simplejson >= 2.0.9"
 PYTHONPATH=. easy_install -a -Z -d . "formencode>=1.2.2"
 
 for egg in *.egg
