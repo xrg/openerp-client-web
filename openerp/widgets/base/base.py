3a113d529ce337ab515451ab9445c9a9f52fb1bf
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-05-04 14:55:32 +0530

    Fixed base widget (display/render problem).

--- openerp/widgets/base/base.py
+++ openerp/widgets/base/base.py
@@ -231,8 +231,11 @@ class Widget(object):
         kw = self.prepare_dict(value, kw)
         output = tools.renderer(self.template_c)(**kw)
         return output
-
-    __call__ = display = render
+    
+    def display(self, value=None, **kw):
+        return self.render(value, **kw)
+    
+    __call__ = display
 
     def get_default(self):
         if callable(self.default):
