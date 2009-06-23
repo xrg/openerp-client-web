67444f20874ad0d75d1c6bc310360ef1e151be43
noz <noz@tinyerp.com>
2009-06-23 12:52:23 +0530

    * [FIX] Fixed pie chart label.

--- openerp/widgets/graph.py
+++ openerp/widgets/graph.py
@@ -579,7 +579,11 @@ class PieChart(GraphData):
             val['text'] = x
             val['label'] = x
             val['on-click'] = "function(){onChartClick('" + url[i] + "')}"
-            val["tip"] = x + ' (' + str(round((100 * value[i])/total_val)) + '%)'
+            
+            if total_val <> 0.0:
+                val["tip"] = x + ' (' + str(round((100 * value[i])/total_val)) + '%)'
+            else:
+                val["tip"] = x
 
             allvalues.append(val)
             
