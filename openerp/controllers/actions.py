7e712b39cede8a0d0cb3f8411883e8d9109d37b6
ame (Tiny/Axelor) <ame@tinyerp.com>
2009-08-10 14:34:20 +0530

    [FIX] action controller (invalid active id).

--- openerp/controllers/actions.py
+++ openerp/controllers/actions.py
@@ -233,8 +233,8 @@ def execute(action, **data):
             action['domain']='[]'
 
         ctx = data.get('context', {}).copy()
-        ctx.update({'active_id': data.get('id', False), 'active_ids': data.get('ids', [])})
         ctx.update(tools.expr_eval(action.get('context', '{}'), ctx.copy()))
+        ctx.update({'active_id': data.get('id', False), 'active_ids': data.get('ids', [])})
 
         # save active_id in session
         rpc.session.active_id = data.get('id')
