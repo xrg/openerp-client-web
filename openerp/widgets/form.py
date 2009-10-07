a2621109acfa52803a760bf31a1455f3888b6627
noz@tinyerp.com <>
2009-10-07 11:34:28 +0530

    [FIX] Buttons are not clickable when readonly='1'.

--- openerp/widgets/form.py
+++ openerp/widgets/form.py
@@ -572,9 +572,12 @@ class Button(TinyInputWidget):
         self.string = re.sub('_(?!_)', '', self.string or '')
 
         self.btype = attrs.get('special', attrs.get('type', 'workflow'))
-
+        
         self.nolabel = True
-        self.readonly = False
+        self.readonly = attrs.get('readonly', False)
+        
+        if self.readonly:
+            attrs['disabled'] = 'disabled'
 
         if self.icon:
             self.icon = icons.get_icon(self.icon)
