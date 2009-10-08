1bfdce9e32a3f0752aef182aecfbd1765a026567
noz@tinyerp.com <>
2009-10-08 18:30:17 +0530

    [FIX] Improved link='1' for M2O.

--- openerp/widgets/listgrid.py
+++ openerp/widgets/listgrid.py
@@ -413,7 +413,11 @@ class M2O(Char):
 
     template = """\
         <span>
-            <a href="${link}">${text}</a>
+            % if link:
+                <a href="${link}">${text}</a>
+            % else:
+                ${text}
+            % endif
         </span>
     """
 
@@ -429,7 +433,12 @@ class M2O(Char):
         return ''
 
     def get_link(self):
-        return tools.url('/form/view', model=self.attrs['relation'], id=(self.value or False) and self.value[0])
+        m2o_link = int(self.attrs.get('link', 1))
+        
+        if m2o_link == 1:
+            return tools.url('/form/view', model=self.attrs['relation'], id=(self.value or False) and self.value[0])
+        else:
+            return None
 
 class O2M(Char):
 
