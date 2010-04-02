3ef4cdf2de5a00967c0aac7d547042dee4ed8482
Xavier Morel <xmo@tinyerp.com>
2010-04-02 11:30:02 +0200

    [IMP] fix a few warnings, use named functions (no need to terminate statement + better debugger & profiler support)

--- addons/openerp/static/javascript/search.js
+++ addons/openerp/static/javascript/search.js
@@ -28,15 +28,14 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function add_filter_row() {
-
     var filter_table = $('filter_table');
     var vals = [];
     var row_id = 1;
-	
+
     var first_row = $('filter_row/0');
     var trs = MochiKit.DOM.getElementsByTagAndClassName('tr', null, filter_table);
     var old_qstring;
-	
+
     if (filter_table.style.display == 'none') {
         filter_table.style.display = '';
     } else if (first_row.style.display == 'none' && trs.length <= 1) {
@@ -50,65 +49,67 @@ function add_filter_row() {
         var old_tr = trs[trs.length - 1];
         old_qstring = MochiKit.DOM.getFirstElementByTagAndClassName('input', 'qstring', old_tr);
         old_qstring.style.background = '#FFFFFF';
-	
+
         var new_tr = old_tr.cloneNode(true);
         keys = new_tr.id.split('/');
         id = parseInt(keys[1], 0);
 		row_id = id + row_id;
 
-		new_tr.id =  keys[0] +'/'+ row_id;
-		var filter_column = MochiKit.DOM.getFirstElementByTagAndClassName('td', 'filter_column', new_tr);
-		
-		var filter_fields = MochiKit.DOM.getFirstElementByTagAndClassName('select', 'filter_fields', new_tr);
-		var expr = MochiKit.DOM.getFirstElementByTagAndClassName('select', 'expr', new_tr);
-		var qstring = MochiKit.DOM.getFirstElementByTagAndClassName('input', 'qstring', new_tr);
-		
-		filter_column.id = filter_column.id.split('/')[0] + '/' + row_id;
-		filter_fields.id = filter_fields.id.split('/')[0] + '/' + row_id;
-		expr.id = expr.id.split('/')[0] + '/' + row_id;
-		qstring.id = qstring.id.split('/')[0] + '/' + row_id;
-		qstring.style.background = '#FFFFFF';
-		qstring.value = '';
-		
-		var image_col = MochiKit.DOM.getFirstElementByTagAndClassName('td', 'image_col', new_tr);
-		image_col.id = 'image_col/' + row_id
-		
+        new_tr.id = keys[0] + '/' + row_id;
+        var filter_column = MochiKit.DOM.getFirstElementByTagAndClassName('td', 'filter_column', new_tr);
+
+        var filter_fields = MochiKit.DOM.getFirstElementByTagAndClassName('select', 'filter_fields', new_tr);
+        var expr = MochiKit.DOM.getFirstElementByTagAndClassName('select', 'expr', new_tr);
+        var qstring = MochiKit.DOM.getFirstElementByTagAndClassName('input', 'qstring', new_tr);
+
+        filter_column.id = filter_column.id.split('/')[0] + '/' + row_id;
+        filter_fields.id = filter_fields.id.split('/')[0] + '/' + row_id;
+        expr.id = expr.id.split('/')[0] + '/' + row_id;
+        qstring.id = qstring.id.split('/')[0] + '/' + row_id;
+        qstring.style.background = '#FFFFFF';
+        qstring.value = '';
+
+        var image_col = MochiKit.DOM.getFirstElementByTagAndClassName('td', 'image_col', new_tr);
+        image_col.id = 'image_col/' + row_id;
+
         var old_and_or = MochiKit.DOM.getFirstElementByTagAndClassName('td', 'and_or', new_tr);
         if (old_and_or) {
             removeElement(old_and_or);
         }
-		
-		var and_or = MochiKit.DOM.createDOM('td');
-		and_or.id = 'and_or/' + id;
-		and_or.className = 'and_or';
-		
-		var select_andor = MochiKit.DOM.createDOM('select');
-		select_andor.id = 'select_andor/' + id;
-		select_andor.className = 'select_andor';
-		
-		var option = MochiKit.DOM.createDOM('option');
-		
-		vals.push('AND');
-		vals.push('OR');
-		
-		option = map(function(x){return OPTION({'value': x}, x)}, vals);
-		image_replace = openobject.dom.get('image_col/'+ id);
-		if(MochiKit.DOM.getFirstElementByTagAndClassName('td', 'and_or', old_tr) == null){
-			insertSiblingNodesBefore(image_replace, and_or)
-		}
-		
-		appendChildNodes(select_andor, option);
-		appendChildNodes(and_or, select_andor);
-		insertSiblingNodesAfter(old_tr, new_tr);
-	}
+
+        var and_or = MochiKit.DOM.createDOM('td');
+        and_or.id = 'and_or/' + id;
+        and_or.className = 'and_or';
+
+        var select_andor = MochiKit.DOM.createDOM('select');
+        select_andor.id = 'select_andor/' + id;
+        select_andor.className = 'select_andor';
+
+        var option = MochiKit.DOM.createDOM('option');
+
+        vals.push('AND');
+        vals.push('OR');
+
+        var options = map(function(x) {
+            return OPTION({'value': x}, x)
+        }, vals);
+        image_replace = openobject.dom.get('image_col/' + id);
+        if (MochiKit.DOM.getFirstElementByTagAndClassName('td', 'and_or', old_tr) == null) {
+            insertSiblingNodesBefore(image_replace, and_or)
+        }
+
+        appendChildNodes(select_andor, options);
+        appendChildNodes(and_or, select_andor);
+        insertSiblingNodesAfter(old_tr, new_tr);
+    }
 }
 
 function remove_row(id) {
-	
+
     var filter_table = $('filter_table');
-	
-    node = MochiKit.DOM.getFirstParentByTagAndClassName(id, 'tr', 'filter_row_class');
-	
+
+    var node = MochiKit.DOM.getFirstParentByTagAndClassName(id, 'tr', 'filter_row_class');
+
     if (node.id != 'filter_row/0') {
         removeElement(node);
     }
@@ -129,13 +130,13 @@ function search_image_filter(src, id) {
 
 function onKey_Event() {
     var search_filter = $('search_filter_data');
-	
+
     var editors = [];
-	
+
     editors = editors.concat(getElementsByTagAndClassName('input', null, search_filter));
     editors = editors.concat(getElementsByTagAndClassName('select', null, search_filter));
     editors = editors.concat(getElementsByTagAndClassName('textarea', null, search_filter));
-    
+
     var active_editors = filter(function(e) {
         return e.type != 'hidden' && !e.disabled
     }, editors);
@@ -146,9 +147,7 @@ function onKey_Event() {
 }
 
 function onKeyDown_search(evt) {
-    var key = evt.key();
-    
-    if (key.string == "KEY_ENTER") {
+    if (evt.key().string == "KEY_ENTER") {
         search_filter();
     }
 }
@@ -159,7 +158,7 @@ function search_filter(src, id) {
     domains = {};
     search_context = {};
     var group_by_ctx = [];
-	
+
     domain = 'None';
     if (src) {
         src.checked = !src.checked;
@@ -167,7 +166,7 @@ function search_filter(src, id) {
     }
     var filter_table = $('filter_table');
     datas = $$('[name]', 'search_filter_data');
-	
+
     forEach(datas, function(d) {
         if (d.type != 'checkbox' && d.name && d.value && d.name.indexOf('_terp_') == -1 && d.name != 'filter_list') {
             value = d.value;
@@ -181,14 +180,14 @@ function search_filter(src, id) {
             domains[d.name] = value;
         }
     });
-	
+
     domains = serializeJSON(domains);
     all_domains['domains'] = domains;
     all_domains['search_context'] = search_context;
     selected_boxes = getElementsByTagAndClassName('input', 'grid-domain-selector');
-	
+
     all_boxes = [];
-	
+
     forEach(selected_boxes, function(box) {
         if (box.id && box.checked && box.value != '[]') {
             all_boxes = all_boxes.concat(box.value);
@@ -198,36 +197,36 @@ function search_filter(src, id) {
             group_by_ctx = group_by_ctx.concat(group);
         }
     });
-	
+
     openobject.dom.get('_terp_group_by_ctx').value = group_by_ctx;
-	
+
     checked_button = all_boxes.toString();
-	
+
     if (checked_button.length > 0) {
         check_domain = checked_button.replace(/(]\,\[)/g, ', ');
     }
     else {
         check_domain = 'None';
     }
-	
+
     all_domains['check_domain'] = check_domain;
-	
+
     var selection_domain = $('filter_list').value;
-	
+
     if (selection_domain) {
         all_domains['selection_domain'] = selection_domain;
     }
-	
+
     if (openobject.dom.get('_terp_filter_domain').value != '[]') {
-	
+
         var params = {};
         var record = {};
-		
+
         filter_table.style.display = '';
-		
+
         children = MochiKit.DOM.getElementsByTagAndClassName('tr', 'filter_row_class', filter_table);
         forEach(children, function(ch) {
-		
+
             var ids = ch['id'];	// row id...
             var id = ids.split('/')[1];
             var qid = 'qstring/' + id;
@@ -241,12 +240,12 @@ function search_filter(src, id) {
                 record[ids] = rec;
             }
         });
-			
+
         record = serializeJSON(record);
         params['record'] = record;
-		
+
         var search_req = openobject.http.postJSON('/search/get', params);
-		
+
         var custom_domain = [];
         search_req.addCallback(function(obj) {
             if (obj.error) {
@@ -269,7 +268,7 @@ function search_filter(src, id) {
                     row_id = serializeJSON(i);
                     id = row_id.split('/')[1];
                     id = parseInt(id, 10);
-    				
+
                     var eid = 'expr/' + id;
                     var select_andor = 'select_andor/' + id;
                     var type = obj.frm[i].type;
@@ -283,7 +282,7 @@ function search_filter(src, id) {
                     if (operator != 'None') {
                         temp_domain.push(operator);
                     }
-					
+
                     var first_text = obj.frm[i].rec;
                     var expression = $(eid).value;
                     var right_text = obj.frm[i].rec_val;
@@ -302,17 +301,17 @@ function search_filter(src, id) {
                     if (expression == 'in' || expression == 'not in') {
                         right_text = right_text.split(',');
                     }
-    				
+
                     temp_domain.push(first_text);
                     temp_domain.push(expression);
                     temp_domain.push(right_text);
-    				
+
                     custom_domain.push(temp_domain);
                 }
             }
             custom_domain = serializeJSON(custom_domain);
             all_domains = serializeJSON(all_domains);
-					
+
             final_search_domain(custom_domain, all_domains, group_by_ctx);
         });
     } else {
@@ -330,7 +329,7 @@ function final_search_domain(custom_domain, all_domain, group_by_ctx) {
         all_domains: all_domains,
         group_by_ctx: group_by_ctx
     });
-	
+
     req.addCallback(function(obj) {
         if (obj.flag) {
             var params = {'domain': obj.sf_dom,
