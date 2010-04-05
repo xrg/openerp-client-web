fcb3790c7251e2f6dac189d8fbc408c018bd1f67
nva (OpenERP) <nva@tinyerp.com>
2010-04-05 14:42:26 +0530

    [Add] Addons CKEditor

--- addons/widget_fck/__terp__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name" : "FCK editor widget for OpenERP",
-    "version" : "1.0",
-    "depends" : ["openerp"],
-    "author" : "Tiny",
-    "description": """FCK editor widget for OpenERP.""",
-    'website': 'http://www.openerp.com/',
-    'active': False,
-}
