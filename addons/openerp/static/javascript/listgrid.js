92de1d76a1bd0781b6e3e553e504f98ee0f208e5
Xavier Morel <xmo@tinyerp.com>
2010-04-09 10:35:25 +0200

    [IMP] remove $ alias, inline jquery global

--- addons/openerp/static/javascript/listgrid.js
+++ addons/openerp/static/javascript/listgrid.js
@@ -620,21 +620,20 @@ MochiKit.Base.update(ListView.prototype, {
             }
 
             MochiKit.Signal.signal(__listview, 'onreload');
-            
+
             //Make all records Editable by Double-click
-            var $ = jQuery;
-            var view_type = $('[id*=_terp_view_type]').val();
-            var editable = $('[id*=_terp_editable]').val();
-            
-            $('table[id^="'+table+'"].grid tr.grid-row').each(function(e) { 
-            	$(this).dblclick(function(event) {
+            var view_type = jQuery('[id*=_terp_view_type]').val();
+            var editable = jQuery('[id*=_terp_editable]').val();
+
+            jQuery('table[id^="'+table+'"].grid tr.grid-row').each(function(e) {
+            	jQuery(this).dblclick(function(event) {
             		if (!(event.target.className == 'checkbox grid-record-selector' || event.target.className == 'listImage')) {
             			if (view_type == 'tree') {
             				if (editable != 'True') {
-            					do_select($(this).attr('record'));
+            					do_select(jQuery(this).attr('record'));
             				}
             				else {
-            					editRecord($(this).attr('record'));
+            					editRecord(jQuery(this).attr('record'));
             				}
             			}
             		}
