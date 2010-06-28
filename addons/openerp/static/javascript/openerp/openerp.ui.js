90f1cc8c150e1e8b0a20984f57a976a311b2d65f
Xavier Morel <xmo@openerp.com>
2010-06-28 16:01:13 +0200

    [IMP] move sidebar toggling into openerp.ui.js (from python template)

--- addons/openerp/static/javascript/openerp/openerp.ui.js
+++ addons/openerp/static/javascript/openerp/openerp.ui.js
@@ -36,3 +36,44 @@ if (typeof(openerp) == "undefined") {
 }
 
 openerp.ui = {};
+
+function toggle_sidebar(forced) {
+    function a() {
+        var sb = openobject.dom.get('sidebar');
+
+        sb.style.display = forced ? forced : (sb.style.display == "none" ? "" : "none");
+        openobject.http.setCookie("terp_sidebar", sb.style.display);
+
+        var a_img = openobject.dom.get('toggle-click');
+        var tertiary = openobject.dom.get('tertiary');
+        var tertiary_wrap = openobject.dom.get('tertiary_wrap');
+        var sidebar_hide = openobject.dom.get('sidebar_hide');
+        var attach_sidebar = openobject.dom.get('attach_sidebar');
+        var add_attachment = openobject.dom.get('add_attachment');
+        var customise_menu = openobject.dom.get('customise_menu');
+        if (sb.style.display == "none") {
+            setNodeAttribute(a_img, 'class', 'off');
+            setNodeAttribute(tertiary, 'style', 'width: 21px');
+            setNodeAttribute(tertiary_wrap, 'style', 'padding: 0 0 0 0');
+            setNodeAttribute(sidebar_hide, 'style', 'padding: 0 0 0 0');
+            setNodeAttribute(attach_sidebar, 'style', 'display: none');
+            setNodeAttribute(add_attachment, 'style', 'display: none');
+            setNodeAttribute(customise_menu, 'style', 'display: none');
+        } else {
+            setNodeAttribute(a_img, 'class', 'on');
+            setNodeAttribute(tertiary, 'style', 'width: 180px');
+            setNodeAttribute(tertiary_wrap, 'style', 'padding: 0 0 0 10px');
+            setNodeAttribute(sidebar_hide, 'style', 'padding: 0 0 0 8px');
+            setNodeAttribute(attach_sidebar, 'style', "display: ''");
+            setNodeAttribute(add_attachment, 'style', "display: ''");
+            setNodeAttribute(customise_menu, 'style', "display: ''");
+        }
+    }
+    if (typeof(Notebook) == "undefined") {
+        a();
+    } else {
+        Notebook.adjustSize(a);
+    }
+
+    MochiKit.Signal.signal(document, 'toggle_sidebar');
+}
