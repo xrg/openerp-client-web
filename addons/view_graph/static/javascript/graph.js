66c97bbee05f7701a30f4e6d1bdd9fc333214926
Xavier Morel <xmo@openerp.com>
2010-07-09 08:11:04 +0200

    [ADD] forgotten graph.js file

---
+++ addons/view_graph/static/javascript/graph.js
@@ -0,0 +1,18 @@
+var charts = {};
+function get_chart(id) {
+    return MochiKit.Base.serializeJSON(charts[id]);
+}
+function setup_charts() {
+    jQuery('div.flash-chart').each(function (index, e) {
+        var name = jQuery(e).attr('id');
+        var chart_id = name.slice(0, -1);
+
+        swfobject.embedSWF(openobject.http.getURL("/view_graph/static/open-flash-chart.swf"), name, 500, 350, "9.0.0",
+            "expressInstall.swf", {'get-data': 'get_chart', 'id': chart_id}, {'wmode': 'transparent'});
+    });
+}
+function onChartClick(path) {
+    openobject.http.redirect(path);
+}
+
+jQuery(document).bind('ready ajaxStop', setup_charts);
