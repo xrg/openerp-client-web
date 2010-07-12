60edeaa4767b53d1854042a5e623588e8b9479e0
vda (Open ERP) <vda@tinyerp.com>
2010-07-12 16:27:03 +0530

    [FIX] Fix filters issue.

--- addons/openerp/widgets/search.py
+++ addons/openerp/widgets/search.py
@@ -170,6 +170,9 @@ class Filter(TinyInputWidget):
         self.text_val = self.string or self.help
         if self.icon:
             self.icon = icons.get_icon(self.icon)
+            
+        if self.string == self.help:
+            self.help = None
 
 class M2O_search(M2O):
     template = """
