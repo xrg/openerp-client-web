014daf96e20bd176ddf7927d7fda29ee7c3938c1
vda (Open ERP) <vda@tinyerp.com>
2010-07-14 16:22:49 +0530

    [FIX] Fix Search filters issue.

--- addons/openerp/widgets/form/_form.py
+++ addons/openerp/widgets/form/_form.py
@@ -77,12 +77,7 @@ class Frame(TinyInputWidget):
         self.table = []
 
         self.add_row()
-        self.filter_boxes = [] # Used for make Filter button group osx box
-        from openerp.widgets.search import Filter
-        for child in range(len(self.children)):
-            if isinstance(self.children[child], Filter):
-                self.filter_boxes.append(child)
-                
+        
         for child in self.children:
             
             string = not child.nolabel and child.string
@@ -111,32 +106,8 @@ class Frame(TinyInputWidget):
             self.table[0] = [(a, w) for a, w in self.table[0] if getattr(w, 'visible', 1)]
 
         max_length = max([len(row) for row in self.table])
-        
         for row in self.table:
-            if len(row) == len(self.filter_boxes) and row:
-                row[0][0]['class'] = row[0][0]['class'] + ' first_box'
-                row[-1][0]['class'] = row[-1][0]['class'] + ' last_box'
-            else:
-                for ch in range(len(row)):
-                    if ch in self.filter_boxes:
-                        if isinstance(row[ch][1], Filter):
-                            if ch == 0:
-                                row[ch][0]['class'] = row[ch][0]['class'] + ' first_box'
-                            else:
-                                if ch == len(row) -1 and ch in self.filter_boxes:
-                                    if ch -1 not in self.filter_boxes and row[ch][1].string:
-                                        row[ch][0]['class'] = row[ch][0]['class'] + ' first_box'
-                                    if row[ch][1].string:
-                                        row[ch][0]['class'] = row[ch][0]['class'] + ' last_box'
-                                    
-                    else:
-                        if ch > 0 and ch-1 in self.filter_boxes:
-                            if isinstance(row[ch-1][1], Filter) and row[ch -1][1].string :
-                                row[ch -1][0]['class'] = row[ch -1][0]['class'] + ' last_box'
-                            
-                    
-                        
-                
+            
             sn = len([w for a, w in row if isinstance(w, (basestring, Label, Image))])
             sw = 5                                  # label & image width
             ww = 100.00 - sw * sn                   # remaining width
@@ -249,7 +220,10 @@ class Frame(TinyInputWidget):
             if self.label_position and (not (widget.kind or widget._name)) or (isinstance(widget, Filter) and widget.string):
                 if isinstance(widget, Filter):
                     attrs['class'] = attrs.get('class', 'item') + ' search_filters group_box'
-                    
+                    if getattr(widget, 'first_box'):
+                        attrs['class'] = attrs['class'] + ' first_box'
+                    if getattr(widget, 'last_box'):
+                        attrs['class'] = attrs['class'] + ' last_box'
                     if widget.def_checked:
                         attrs['class'] = attrs['class'] + ' grop_box_active'
                 else:
