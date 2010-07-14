83114aac2c7ad291723d43e3bc804241e12aa603
Amit Mendapara <mendapara.amit@gmail.com>
2010-07-14 20:12:52 +0530

    [IMP] Can override cherrypy server host/port from command line.

--- openobject/commands.py
+++ openobject/commands.py
@@ -23,6 +23,8 @@ def start():
     parser = OptionParser(version="%s" % (openobject.release.version))
     parser.add_option("-c", "--config", metavar="FILE", dest="config",
                       help="configuration file", default=get_config_file())
+    parser.add_option("-a", "--address", help="host address, overrides server.socket_host")
+    parser.add_option("-p", "--port", help="port number, overrides server.socket_port")
     parser.add_option("--no-static", dest="static",
                       action="store_false", default=True,
                       help="Disables serving static files through CherryPy")
@@ -31,11 +33,21 @@ def start():
     if not os.path.exists(options.config):
         raise ConfigurationError(_("Could not find configuration file: %s") %
                                  options.config)
-
+                                 
     app_config = as_dict(options.config)
+    
     openobject.configure(app_config)
     if options.static:
         openobject.enable_static_paths()
-
+    
+    if options.address:
+        cherrypy.config['server.socket_host'] = options.address
+    if options.port:
+        try:
+            cherrypy.config['server.socket_port'] = int(options.port)
+        except:
+            pass
+    
     cherrypy.engine.start()
     cherrypy.engine.block()
+    
