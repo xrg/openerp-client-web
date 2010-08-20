a5fd7c2c01ac8d4b9f61e73b55bfaf133dc98f62
vda (Open ERP) <vda@tinyerp.com>
2010-08-20 18:08:58 +0530

    [FIX] Fixed loading waitbox for treeview.

--- addons/openerp/static/javascript/ajax_stat.js
+++ addons/openerp/static/javascript/ajax_stat.js
@@ -29,11 +29,10 @@
 
 
 (function() {
-    var __ajax_stat_elem = null;
+    var waitBox = null;
 
     var onAjaxStart = function() {
-        onAjaxStatPosition();
-        MochiKit.DOM.showElement(__ajax_stat_elem);
+		waitBox.showAfter(100);
     };
 
     var onAjaxStop = function() {
@@ -42,43 +41,18 @@
             return;
 
         try {
-            MochiKit.Async.callLater(0.5, MochiKit.DOM.hideElement, __ajax_stat_elem);
+        	setTimeout(jQuery.proxy(waitBox, 'hide'), 1)
         } catch(e) {
         }
     };
 
-    var onAjaxStatPosition = function() {
-
-        var x = (MochiKit.DOM.getViewportDimensions().w / 2) -
-                (MochiKit.DOM.elementDimensions(__ajax_stat_elem).w / 2);
-        var y = (window.pageYOffset ||
-                parent.document.body.scrollTop ||
-                parent.document.documentElement.scrollTop) + 5;
-
-        __ajax_stat_elem.style.left = x + 'px';
-        __ajax_stat_elem.style.top = y + 'px';
-    };
-
     jQuery(document).ready(function() {
-        __ajax_stat_elem = DIV({'align': 'center'}, _('Loading...'));
-
-        with (__ajax_stat_elem.style) {
-            display = "none";
-            position = "absolute";
-            padding = "2px 8px";
-            color = "white";
-            backgroundColor = "#9a0404";
-            fontWeight = "bold";
-            zIndex = 1000;
-        }
-
-        MochiKit.DOM.appendChildNodes(parent.document.body, __ajax_stat_elem);
+        waitBox = new openerp.ui.WaitBox();
 
         MochiKit.Signal.connect(window, "ajaxStart", onAjaxStart);
         MochiKit.Signal.connect(window, "ajaxStop", onAjaxStop);
 
-        MochiKit.Signal.connect(window, "onresize", onAjaxStatPosition);
-        MochiKit.Signal.connect(window, "onscroll", onAjaxStatPosition);
-        onAjaxStatPosition();
+//        MochiKit.Signal.connect(window, "onresize", onAjaxStatPosition);
+//        MochiKit.Signal.connect(window, "onscroll", onAjaxStatPosition);
     });
 })();
