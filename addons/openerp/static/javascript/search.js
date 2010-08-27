1c1a8e4bf538f6ef0d1c20468d3bd74f814cc56a
vda (Open ERP) <vda@tinyerp.com>
2010-08-27 10:33:07 +0530

    [FIX] Fixed Search domain issues.

--- addons/openerp/static/javascript/search.js
+++ addons/openerp/static/javascript/search.js
@@ -330,6 +330,7 @@ function final_search_domain(custom_domain, all_domains, group_by_ctx) {
 			 }
 			 
 			 if (obj.domain) { // For direct search
+				
 			 	var in_req = eval_domain_context_request({
 					source: '_terp_list', 
 					domain: obj.domain, 
@@ -338,6 +339,7 @@ function final_search_domain(custom_domain, all_domains, group_by_ctx) {
 				});
 				
 				in_req.addCallback(function(in_obj){
+					openobject.dom.get('_terp_domain').value = obj.domain;
 			    	openobject.dom.get('_terp_search_domain').value = in_obj.domain;
 			    	openobject.dom.get('_terp_search_data').value = obj.search_data;
 			    	openobject.dom.get('_terp_context').value = in_obj.context;
