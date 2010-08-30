e8706731bfb720bad4d6e0af3a5d1d7984a5b3ec
vda (Open ERP) <vda@tinyerp.com>
2010-08-30 11:27:20 +0530

    [FIX] Fixed graph width to 100%.

--- addons/view_graph/static/javascript/graph.js
+++ addons/view_graph/static/javascript/graph.js
@@ -8,7 +8,7 @@ function setup_charts() {
 	jQuery('div.flash-chart').each(function (index, e) {
 		var name = jQuery(e).attr('id');
 		var chart_id = name.slice(0, -1);
-		swfobject.embedSWF(openobject.http.getURL("/view_graph/static/open-flash-chart.swf"), name, 500, 350, "9.0.0",
+		swfobject.embedSWF(openobject.http.getURL("/view_graph/static/open-flash-chart.swf"), name, '100%', 350, "9.0.0",
 			"expressInstall.swf", {'get-data': 'get_chart', 'id': chart_id}, {'wmode': 'transparent'});
 		
 		if(index == (last_chart -1)) {
