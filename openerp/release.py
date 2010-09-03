f28a5a03d251300d0d445d6824f2d7245ea80aac
Stephane Wirtel <stephane@openerp.com>
2010-09-03 18:24:13 +0200

    [RELEASE] OpenERP 5.0.14

--- openerp/release.py
+++ openerp/release.py
@@ -29,7 +29,7 @@
 
 # Release information about openerp-web
 
-version = "5.0.13"
+version = "5.0.14"
 
 description = "Web Client of OpenERP, the Enterprise Management Software"
 long_description = "OpenERP Web is the web client of the OpenERP, a free enterprise management software: accounting, stock, manufacturing, project mgt..."
