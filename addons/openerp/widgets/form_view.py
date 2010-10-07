1ceebe0fc1dcbc360c3a2a71b01b66fb84c3e39d
vda (Open ERP) <vda@tinyerp.com>
2010-10-07 11:38:42 +0530

    [FIX] Sidebar display only in views, not in wizards.

--- addons/openerp/widgets/form_view.py
+++ addons/openerp/widgets/form_view.py
@@ -98,7 +98,8 @@ class ViewForm(Form):
         if self.screen.widget and hasattr(self.screen.widget, 'sidebar'):
             self.sidebar = self.screen.widget.sidebar
         else:
-            self.sidebar = Sidebar(self.screen.model, self.screen.submenu, self.screen.toolbar, self.screen.id,
+            if self.screen.toolbar:
+                self.sidebar = Sidebar(self.screen.model, self.screen.submenu, self.screen.toolbar, self.screen.id,
                                self.screen.view_type, context=self.screen.context)
 
         if params.view_type == 'tree':
