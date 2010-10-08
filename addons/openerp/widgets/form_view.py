6dc40e57b55470cfdf81a6d60695017911747301
vda (Open ERP) <vda@tinyerp.com>
2010-10-08 15:49:14 +0530

    [FIX] Sidebar option.

--- addons/openerp/widgets/form_view.py
+++ addons/openerp/widgets/form_view.py
@@ -98,7 +98,8 @@ class ViewForm(Form):
         if self.screen.widget and hasattr(self.screen.widget, 'sidebar'):
             self.sidebar = self.screen.widget.sidebar
         else:
-            if self.screen.toolbar:
+            view_mode = self.screen.view_mode
+            if  'form' in view_mode and view_mode.index('form') > 0:
                 self.sidebar = Sidebar(self.screen.model, self.screen.submenu, self.screen.toolbar, self.screen.id,
                                self.screen.view_type, context=self.screen.context)
 
