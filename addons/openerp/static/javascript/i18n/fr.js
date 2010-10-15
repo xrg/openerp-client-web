6c5e07643a889fdae1799600d124a14a96cc1526
noz (Open ERP) <noz@tinyerp.com>
2010-10-15 13:14:25 +0530

    [IMP] Testing implementation for translation.

---
+++ addons/openerp/static/javascript/i18n/fr.js
@@ -0,0 +1,6 @@
+
+// Auto generated file. Please don't modify.
+openobject.gettext.update(
+{"Do you really want to delete record ?": "Do you really want to delete record ?", "Do you really want to delete selected record(s) ?": "Do you really want to delete selected record(s) ?", "You selected to open %(tabs)s tabs - do you want to continue?": "You selected to open %(tabs)s tabs - do you want to continue?", "Do you really want to delete this record?": "Do you really want to delete this record?", "Invalid form, correct red fields.": "INVALIDDDDDDDDD", "Do you really wants to create an inherited view here?": "Do you really wants to create an inherited view here?", "Do you really want to remove this node?": "Do you really want to remove this node?", "You must select at least one record.": "You must select at least one record."}
+);
+
