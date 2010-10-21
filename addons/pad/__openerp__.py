fd15c5c979383c09d861b6e397277f3260696e28
Xavier Morel <xmo@openerp.com>
2010-10-21 11:23:09 +0200

    [REM] pad addons: move to server modules

--- addons/pad/__openerp__.py
+++
@@ -1,8 +0,0 @@
-{
-    "name" : "(Ether)Pad enhanced support",
-    "version" : "1.0",
-    "depends" : ["openerp"],
-    "author" : "Tiny",
-    "website": 'http://www.openerp.com/',
-    "active": True,
-}
