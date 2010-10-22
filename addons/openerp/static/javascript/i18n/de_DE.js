8f9e068fdba3b30aebdf5078c0f2398cf9d1fed8
noz (Open ERP) <noz@tinyerp.com>
2010-10-22 15:37:55 +0530

    [REM] Removed auto generated po files.

--- addons/openerp/static/javascript/i18n/de_DE.js
+++
@@ -1,6 +0,0 @@
-
-// Auto generated file. Please don't modify.
-openobject.gettext.update(
-{"Do you really want to delete selected record(s) ?": "Wollen Sie die ausgew\u00e4hlten Datens\u00e4tze wirklich l\u00f6schen", "You selected to open %(tabs)s tabs - do you want to continue?": "You selected to open %(tabs)s tabs - do you want to continue?", "Do you really want to delete this record?": "Wollen Sie diesen Datensatz wirklich l\u00f6schen?", "Invalid form, correct red fields !": "ung\u00fcltiges Formular, korrigieren Sie die roten Felder!", "Do you really wants to create an inherited view here?": "Wollen Sie hier wirklich eine vererbte Sicht erstellen?", "Do you really want to remove this node?": "Wollen Sie diesen Knoten wirklich l\u00f6schen?", "Loading...": "Ladevorgang l\u00e4uft ...", "You must select at least one record.": "W\u00e4hlen Sie mindestens einen Datensatz"}
-);
-
--- addons/openerp/static/javascript/i18n/es_ES.js
+++
@@ -1,6 +0,0 @@
-
-// Auto generated file. Please don't modify.
-openobject.gettext.update(
-{"Do you really want to delete selected record(s) ?": "\u00bfRealmente desea eliminar el registro(s) seleccionado(s)?", "You selected to open %(tabs)s tabs - do you want to continue?": "You selected to open %(tabs)s tabs - do you want to continue?", "Do you really want to delete this record?": "\u00bfRealmente desea eliminar este registro?", "Invalid form, correct red fields !": "!Formulario inv\u00e1lido, corrija los campos en rojo!", "Do you really wants to create an inherited view here?": "\u00bfRealmente desea crear una vista heredada aqu\u00ed?", "Do you really want to remove this node?": "\u00bfRealmente desea eliminar este nodo?", "Loading...": "Cargando...", "You must select at least one record.": "Debe seleccionar al menos un registro"}
-);
-
