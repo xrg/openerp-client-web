e7c3b6569059594693427c6133d2e70d5b3c2136
Xavier Morel <xmo@openerp.com>
2010-10-25 14:48:08 +0200

    [REM] old addons example (overriding of templates) as we now have actual server addons with template overriding
    or an actual server addon, more precisely

--- addons/form_logger/__openerp__.py
+++
@@ -1,8 +0,0 @@
-{
-    "name" : "Logger of Form Controller methods",
-    "version" : "1.0",
-    "depends" : ["openerp"],
-    "author" : "Tiny",
-    "website": 'http://www.openerp.com/',
-    "active": True,
-}
