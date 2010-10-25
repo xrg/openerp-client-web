76b6089222d8ac131305320119dc5d56d3aecf24
sma (Tiny) <sma@tinyerp.com>
2010-10-25 13:01:28 +0530

    [FIX] Fixed context for logs. - Passed view_id in context (for account.invoice).

--- addons/openerp/widgets/screen.py
+++ addons/openerp/widgets/screen.py
@@ -140,6 +140,8 @@ class Screen(TinyInputWidget):
         else:
             ctx = rpc.session.context.copy()
             ctx.update(self.context)
+            if ctx.get('view_id'):
+                view_id = ctx['view_id']
             view = cache.fields_view_get(self.model, view_id, view_type, ctx, self.hastoolbar, self.hassubmenu)
 
         self.add_view(view, view_type)
