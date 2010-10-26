a763d1b01298b2a213ad0b81bec14ab6ebbd167c
sma (Tiny) <sma@tinyerp.com>
2010-10-26 11:12:33 +0530

    [FIX] Fixed invisible problem for single fields.

--- addons/openerp/static/javascript/form_state.js
+++ addons/openerp/static/javascript/form_state.js
@@ -329,15 +329,17 @@ function form_setVisible(container, field, visible) {
         }
 
     } else {
-        container.style.display = visible ? '' : 'none';
+
         try {
             var label = getNodeAttribute(container, 'for');
             label = MochiKit.Selector.findChildElements(container.parentNode, ['td.label[for="' + label + '"]'])[0];
+
             if (!label){
                 container.style.display = visible ? '' : 'none';
             }
             else{
-                getFirstParentByTagAndClassName(container).style.display = visible ? '' : 'none';
+                jQuery(container).css('display', visible ? '' : 'none')
+                jQuery(container).prev().css('display', visible ? '' : 'none')
             }
         }catch(e){}
     }
