9fa3408e9598b2b2ec10ea2696b086332ce50fa3
Xavier Morel <xmo@openerp.com>
2010-11-26 10:15:59 +0100

    [IMP] add a bit of documentation to the user_home_widget method of the /openerp/widgets controller

--- addons/openerp/controllers/widgets.py
+++ addons/openerp/controllers/widgets.py
@@ -23,6 +23,19 @@ class Widgets(SecuredController):
         return dict(error=error)
 
     def user_home_widgets(self, ctx):
+        """ user_home_widgets(context) -> [{'title':unicode, 'content':unicode, 'removable':bool, 'user_widget_id':object}]
+
+        Returns a list of the widgets to display on the user's home page.
+
+        The widgets list contains both user-widgets (`removable` key set to
+        True) and global widgets (`removable` key set to False), the latter
+        being created and managed by the local adminstrator rather than the
+        current user.
+
+        The `user_widget_id` key is the id of the corresponding
+        `res.widget.user`, which will be deleted if the user removes
+        a widget from his home page (or created if he adds one)
+        """
         widgets = openerp.utils.rpc.RPCProxy('res.widget')
         user_widgets = openerp.utils.rpc.RPCProxy('res.widget.user')
         widget_ids = user_widgets.search(
