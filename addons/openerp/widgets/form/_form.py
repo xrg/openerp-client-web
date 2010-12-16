66acace59e7313afe486bcc5fb55eec233beecee
Fabien Meghazi <fme@openerp.com>
2010-12-16 15:28:16 +0100

    [FIX] Removed legacy code that prevents default values to be set properly

--- addons/openerp/widgets/form/_form.py
+++ addons/openerp/widgets/form/_form.py
@@ -773,14 +773,6 @@ class Form(TinyInputWidget):
         values = {}
         defaults = {}
 
-        # update values according to domain
-        for d in domain:
-            if d[0] in fields:
-                if d[1] == '=':
-                    values[d[0]] = d[2]
-                if d[1] == 'in' and len(d[2]) == 1:
-                    values[d[0]] = d[2][0]
-
         if ids:
             lval = proxy.read(ids[:1], fields.keys() + ['__last_update'], self.context)
             if lval:
