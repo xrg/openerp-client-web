ef56dd0c913931c6a8e55f87d25b82e892e7e357
niv-openerp <nicolas.vanhoren@openerp.com>
2010-12-30 13:31:00 +0100

    [FIX] Bug when a wizard is opened from a form, the form is not refreshed when we close the wizard.

--- addons/openerp/controllers/actions.py
+++ addons/openerp/controllers/actions.py
@@ -359,7 +359,7 @@ def execute(action, **data):
     if 'type' not in action:
         #XXX: in gtk client just returns to the caller
         #raise common.error('Error', 'Invalid action...')
-        return close_popup(reload=False)
+        return close_popup(True)
 
     data.setdefault('context', {}).update(expr_eval(action.get('context','{}'), data.get('context', {}).copy()))
 
