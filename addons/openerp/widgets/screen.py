86d16e4cb35f45b3fa560304c78ac5cf6a406823
sma (Tiny) <sma@tinyerp.com>
2011-01-18 19:06:11 +0530

    [FIX] Fixed view_id when view open from logs.

--- addons/openerp/widgets/screen.py
+++ addons/openerp/widgets/screen.py
@@ -115,6 +115,8 @@ class Screen(TinyInputWidget):
             ctx.update(self.context)
             if ctx.get('view_id'):
                 view_id = ctx['view_id']
+                if 'view_id' in cherrypy.request.terp_params['_terp_context']:
+                    cherrypy.request.terp_params['_terp_context'].pop('view_id')
             view = cache.fields_view_get(self.model, view_id or False, view_type, ctx, self.hastoolbar, self.hassubmenu)
 
         self.add_view(view, view_type)
