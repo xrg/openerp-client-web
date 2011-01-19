2de4f655fe8aae3b9ad41b1a4e7e8e6648e592cc
Fabien Meghazi <fme@openerp.com>
2011-01-19 01:44:06 +0100

    [REVERT] Revert rev#4401

--- addons/openerp/widgets/form_view.py
+++ addons/openerp/widgets/form_view.py
@@ -95,7 +95,7 @@ class ViewForm(Form):
         if self.screen.widget and hasattr(self.screen.widget, 'sidebar'):
             self.sidebar = self.screen.widget.sidebar
         else:
-            if self.screen.toolbar or self.screen.submenu and not self.screen.is_dashboard and not self.screen.is_wizard:
+            if params.target != 'new' and not self.screen.is_dashboard and not  self.screen.is_wizard:
                 self.sidebar = Sidebar(self.screen.model, self.screen.submenu, self.screen.toolbar, self.screen.id,
                                self.screen.view_type, context=self.screen.context)
 
