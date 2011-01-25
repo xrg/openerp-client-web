2f31a2734a60d3b656c542bb2af0512f0cbbd8dc
Thibault Francois <tfr@openerp.com>
2011-01-25 11:28:07 +0100

    [FIX] export : oblige to put quote around all data

--- addons/openerp/controllers/impex.py
+++ addons/openerp/controllers/impex.py
@@ -40,7 +40,7 @@ def datas_read(ids, model, flds, context=None):
 def export_csv(fields, result):
     try:
         fp = StringIO.StringIO()
-        writer = csv.writer(fp)
+        writer = csv.writer(fp, quoting=csv.QUOTE_ALL)
 
         writer.writerow(fields)
 
