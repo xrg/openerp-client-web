742f2d31841a2ef1496967adea41839e8ba905e0
Christophe Simonis <chs@openerp.com>
2011-01-28 11:45:48 +0100

    [FIX] ensure value of char and text widget are not False values

--- addons/openerp/widgets/form/_form.py
+++ addons/openerp/widgets/form/_form.py
@@ -363,7 +363,7 @@ class Char(TinyInputWidget):
         self.validator = validators.String()
 
     def set_value(self, value):
-        self.default = value
+        self.default = value or ''
 
 register_widget(Char, ["char"])
 
@@ -390,7 +390,7 @@ class Text(TinyInputWidget):
         self.validator = validators.String()
 
     def set_value(self, value):
-        self.default = value
+        self.default = value or ''
 
 register_widget(Text, ["text", "text_tag"])
 
