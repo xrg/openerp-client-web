9635704679901ca25dfc9bbd3e76e8840c4e8f9d
Christophe Simonis <chs@openerp.com>
2011-01-31 15:05:36 +0100

    [FIX] name_get do not crash if invalid id

--- addons/openerp/utils/rpc.py
+++ addons/openerp/utils/rpc.py
@@ -434,7 +434,7 @@ def name_get(model, id, context=None):
 
         try:
             name = proxy.name_get([id], ctx)
-            name = name[0][1] or ''
+            name = name and name[0][1] or ''
         except common.TinyWarning:
             name = _("== Access Denied ==")
 
