1e6aa7cc23c31863f67bba4f48951fca90cda9f2
noz (OpenERP) <noz@tinyerp.com>
2011-06-01 13:00:45 +0530

    [FIX] m2m contextmenu doesn't appear in o2m popup ('/' should not be added in prefix).

--- addons/openerp/static/javascript/form.js
+++ addons/openerp/static/javascript/form.js
@@ -802,12 +802,12 @@ function open_search_window(relation, domain, context, source, kind, text){
 function makeContextMenu(id, kind, relation, val){
     var act = get_form_action('get_context_menu');
 
-    var prefix = id.indexOf('/') > -1 ? id.slice(0, id.lastIndexOf('/')) + '/' : '';
-    if ((prefix.split('/')[0])== '_terp_listfields') {
+    var prefix = id.indexOf('/') > -1 ? id.slice(0, id.lastIndexOf('/')) : '';
+    if ((prefix.split('/')[0]) == '_terp_listfields') {
         prefix = (prefix.split('/')[1]);
     }
 
-    var model = prefix ? openobject.dom.get(prefix + '_terp_model').value : openobject.dom.get('_terp_model').value;
+    var model = prefix ? openobject.dom.get(prefix + '/_terp_model').value : openobject.dom.get('_terp_model').value;
 
     openobject.http.postJSON(act, {
         'model': model,
