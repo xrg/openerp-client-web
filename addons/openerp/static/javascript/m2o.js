1ac63e1c953074130efd4897acc26bbd04f15a3a
Chirag Patel(OpenERP) <>
2011-08-04 19:37:40 +0530

    [FIX] Correction over the commit http://bazaar.launchpad.net/~openerp/openobject-client-web/6.0/revision/4671

--- addons/openerp/static/javascript/m2o.js
+++ addons/openerp/static/javascript/m2o.js
@@ -373,7 +373,7 @@ ManyToOne.prototype.get_matched = function() {
     if (jQuery(this.field).hasClass('m2o_search') &&
             this.delayedRequest == null &&
             this.numResultRows == 0 &&
-            !this.text.value) {
+            this.text.value != '') {
         // Allow substring search (press ESC at the combobox)
         return;
     }
