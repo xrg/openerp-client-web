1cffc58aec9806810e496f9b841f59bf979e4ee2
Stephane Wirtel <stw@openerp.com>
2011-08-18 13:16:12 +0200

    [RELEASE] OpenERP 6.0.3

--- openobject/release.py
+++ openobject/release.py
@@ -17,7 +17,7 @@
 #  You can see the MPL licence at: http://www.mozilla.org/MPL/MPL-1.1.html
 #
 name = 'openerp-web'
-version = '6.0.2'
+version = '6.0.3'
 description = "Web Client of OpenERP, the Enterprise Management Software"
 long_description = "OpenERP Web is the web client of the OpenERP, a free enterprise management software"
 author = "OpenERP S.A."
